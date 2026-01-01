ef41bbbcf1bd37f340f52aa4ce651e8ca674a0eb
Poornima Sathsarani <poornimasathsarani62@gmail.com>
2026-01-01 13:08:42 +0530

    Final portfolio website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/ ===================== PRELOADER =====================
+ //===================== PRELOADER =====================
 window.addEventListener("load", () => {
   const preloader = document.getElementById("preloader");
   if (preloader) preloader.style.display = "none";
